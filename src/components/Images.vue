92223178173269cd825dd1585b303126be09a1d4
Michael <mschneider247@gmail.com>
2020-01-14 23:53:54 -0700

    Add additional styling

--- src/components/Images.vue
+++ src/components/Images.vue
@@ -1,10 +1,7 @@
 <template>
-  <div>
-    <h1 id="prompt" v-if='!this.images.length'>Try a search to see some images!</h1>
-    <section class="section_images" v-if='this.images.length' >
-        <img v-for="image in images" :src="image.urls.small" :key="image.id" :alt="image.alt_description" />
-    </section>
-  </div>
+  <section class="section_images" v-if='this.images.length' >
+    <img v-for="image in images" :src="image.urls.small" :key="image.id" :alt="image.alt_description" />
+  </section>
 </template>
 
 <script type="application/javascript">
@@ -17,8 +14,16 @@ export default {
 </script>
 
 <style scoped>
+.section_images {
+  border-radius: 5px;
+  margin-top: 5%;
+  color: white;
+  border: 40px solid #1A0B27;
+}
 img {
-  width: 40%;
+  width: 46%;
+  border-radius: 5px;
+  margin: 2%;
 }
 img:hover {
   transform: scale(1.4); 
